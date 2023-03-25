e6e613a17efc79617727fbc271cba736026322c2
Khuat Tien Quang <quangkt2812@gmail.com>
2023-03-26 00:43:50 +0700

    merge dev

---
+++ CourseManagementWebClient/Models/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CourseManagementWebClientWebClient.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Column(TypeName = "nvarchar")]
+        [StringLength(400)]
+
+        public string? Name { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(400)]
+        public string? Address { get; set; }
+    
+    }
+}
---
+++ CourseManagementWebClient/Models/UserRolesViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CourseManagementWebClientWebClient.Models
+{
+    public class UserRolesViewModel
+    {
+        public string? UserId { get; set; }
+        public string? UserName { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Email { get; set; }
+        public IEnumerable<string>? Roles { get; set; }
+    }
+}
